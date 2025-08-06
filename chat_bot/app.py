399e96c61f8a4e9aac45c8cfc6be571ab74bbd79
yereexx22 <jeremyantonelly241@gmail.com>
2025-08-06 14:49:02 +0000

    Agregar respuesta del chatbot para saludos

--- chat_bot/app.py
+++ chat_bot/app.py
@@ -3,3 +3,5 @@ from flask import Flask,request,jsonify,render_template
 app = Flask(__name__)
 def chatbot_responde(user_mensaje):
     user_message = user_message.lower().strip()
+    if"hola" in user_message or "saludos" in user_message:
+        return "Hola, ¿cómo puedo ayudarte hoy?"
